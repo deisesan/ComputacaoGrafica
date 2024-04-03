25c92adb101b5d83174aa6c5d7aaee3337fa0cd1
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2024-04-03 18:41:01 -0300

    atualizado clear project

--- Trabalho1/src/main.py
+++ Trabalho1/src/main.py
@@ -7,10 +7,10 @@ from gui.MainWindow import MainWindow
 if __name__ == "__main__":
   print("Olá, mundo!")
   readFile = ReadFile("../docs/entrada.xml")
-  writeFile = WriteFile("../docs/saida.xml", readFile.viewport, readFile.window,
-                        readFile.dots, readFile.lines, readFile.polygons)
+  writeFile = WriteFile("../docs/saida.xml", readFile.viewport, readFile.window, readFile.dots, readFile.lines, readFile.polygons)
 
   #Interface Gráfica 
   MainWindow()
   
+  # Limpeza das Pastas de Cache
   ClearProject()
--- Trabalho1/src/utils/ClearProject.py
+++ Trabalho1/src/utils/ClearProject.py
@@ -6,11 +6,11 @@ class ClearProject:
   script_dir = os.path.dirname(os.path.abspath(__file__))
   parent_dir = os.path.dirname(script_dir)
 
-  # pycache_path = os.path.join(parent_dir, "gui/__pycache__")
-  # shutil.rmtree(pycache_path)
   pycache_path = os.path.join(script_dir, "__pycache__")
   shutil.rmtree(pycache_path)
   pycache_path = os.path.join(parent_dir, "models/__pycache__")
+  shutil.rmtree(pycache_path)
+  pycache_path = os.path.join(parent_dir, "gui/__pycache__")
   shutil.rmtree(pycache_path)
   pycache_path = os.path.join(parent_dir, "models/Objects2D/__pycache__")
   shutil.rmtree(pycache_path)
